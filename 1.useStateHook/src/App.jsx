a61899d8e8f69888655be1d0525047b749c476c3
Arnab Gupta <arnabgupta1111@gmail.com>
2023-12-17 23:34:47 +0600

    Simple useState use

--- 1.useStateHook/src/App.jsx
+++ 1.useStateHook/src/App.jsx
@@ -1,9 +1,18 @@
+import { useState } from "react";
 import "./App.css";
 
 function App() {
+  let [count, setCount] = useState(0);
   return (
     <>
-      <h1 className="text-red-400">Vite + React</h1>
+      <div className="w-2/3 min-h-screen flex justify-center items-center mx-auto">
+        <div className="text-center border-2 p-5">
+          <h1 className="text-5xl font-semibold">{count}</h1>
+          <button className="btn mt-4 btn-success" onClick={() => setCount(++count)}>
+            click
+          </button>
+        </div>
+      </div>
     </>
   );
 }
